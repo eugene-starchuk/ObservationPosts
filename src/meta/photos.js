18e48b89d53181d141349b822b2656b82fdade80
Eugene Starchuk <55445382+eugene-starchuk@users.noreply.github.com>
2025-02-12 13:04:37 +0200

    new_img

--- src/meta/photos.js
+++ src/meta/photos.js
@@ -1,22 +1,14 @@
 
 const Photos = [
-    'https://i.imgur.com/RnJW0N2.jpg',
-    'https://i.imgur.com/QnGIdZI.jpg',
-    'https://i.imgur.com/ez03bPv.jpg',
-    'https://i.imgur.com/9E3oH5X.jpg',
-    'https://i.imgur.com/mUSYJzk.jpg',
-    'https://i.imgur.com/KHSL4Ku.jpg',
-    'https://i.imgur.com/cMtmTBh.jpg',
-    'https://i.imgur.com/dstdDKD.jpg',
-    'https://i.imgur.com/GxNvT7R.jpg',
-    'https://i.imgur.com/yfczwvr.jpg',
-    'https://i.imgur.com/Q8ArGrs.jpg',
-    'https://i.imgur.com/RnJW0N2.jpg',
-    'https://i.imgur.com/Pzdrk4Y.jpg',
-    'https://i.imgur.com/F9F7Lma.jpg',
-    'https://i.imgur.com/hbP7bBl.jpg',
-    'https://i.imgur.com/8ZuRV0W.jpg',
-    'https://i.imgur.com/3YvM1S4.jpg',
+    'https://i.imgur.com/Yid74r3.jpg',
+    'https://i.imgur.com/G9zRLKc.jpg',
+    'https://i.imgur.com/eJcCBms.jpg',
+    'https://i.imgur.com/ehGgx3l.jpg',
+    'https://i.imgur.com/auLClb0.jpg',
+    'https://i.imgur.com/KvdJ8Nv.jpg',
+    'https://i.imgur.com/WBMXnnF.jpg',
+    'https://i.imgur.com/sSed5Ep.jpg',
+    'https://i.imgur.com/WWxnaNT.jpg',
 ];
 
 export default Photos;
